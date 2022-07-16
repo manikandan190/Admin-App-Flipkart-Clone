e9f051398c7bc3fe61f87c1084a9072f07f17672
manikandan190 <ramakrishnanmanikandan2000@gmail.com>
2022-07-17 01:07:31 +0530

    first commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,4 @@
-const baseUrl=location.hostname==="localhost"? "http://localhost:2000":"https://flipkart-rest-servers.herokuapp.com"
-export const api=`${baseUrl}/api`;
+export const api='http://localhost:2000/api';
 export const generatePublicUrl=(fileName)=>{
-    return `${baseUrl}/public/${fileName}`;
+    return `http://localhost:2000/public/${fileName}`;
 }
