db0ebd02553a413be0be724779784d4e8b6cd10c
manikandan190 <ramakrishnanmanikandan2000@gmail.com>
2022-07-17 01:20:00 +0530

    first commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,8 @@
-export const api='http://localhost:2000/api';
-export const generatePublicUrl=(fileName)=>{
-    return `http://localhost:2000/public/${fileName}`;
-}
+const baseUrl = process.env.API || "https://flipkart-rest-servers.herokuapp.com";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
